f8ae2252ec0afe537bbfd0653ad87e99cdad6e72
Asif-91 <asifchowdhury323@gmail.com>
2020-07-17 11:19:17 +0600

    starting assignment three

---
+++ assignment.js
@@ -0,0 +1,14 @@
+
+//feetToMile
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+}
+var result = feetToMile(126720);
+console.log(result);
+
+//woodCalculator
+
+//brickCalculator
+
+//tinyFriend
